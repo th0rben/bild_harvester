cb2a42325d8a9ba4c295437a348c598bdae70c5b
Guter Stiel <opi70@web.de>
2018-07-25 12:01:26 +0200

    gitignore

--- bild_harvester/login_data.py
+++
@@ -1,7 +0,0 @@
-sender = "sss@yourdomain.com"
-recipient = "rrr@gmail.com"
-password = "xxxxxx" # Your SMTP password for Gmail
-subject = "Test email from Python"
-text = "Hello from Python"
-server = "smtp.gmail.com"
-port = 465

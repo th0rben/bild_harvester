14369c1de7e457feb324a7b29495a062ec2d77a3
Guter Stiel <opi70@web.de>
2018-07-27 11:23:05 +0200

    improved mail + log file added

--- bild_harvester/main.py
+++ bild_harvester/main.py
@@ -1,11 +1,12 @@
 '''
 @author: th0rben
 '''
-from bild_harvester.html_downloader import create_article_dictionary, find_all_articles
-from bild_harvester.database_maintainer import add_article, initilize_database
 import sqlite3
 import smtplib
 import datetime
+import sys
+from bild_harvester.html_downloader import create_article_dictionary, find_all_articles
+from bild_harvester.database_maintainer import add_article, initilize_database
 from login_data import sender, recipient, password
 from login_data import subject, server, port
 
@@ -13,28 +14,45 @@ def main():
     root_url = 'https://www.bild.de/'
     text = save_articles_sqllite(root_url)
     sendmail(text)
+    #print(text)
 
+# send text as email
 def sendmail(text):
     smtp_server = smtplib.SMTP_SSL(server, port)
     smtp_server.login(sender, password)
     message = "Subject: {}\n\n{}".format(subject, text)
     smtp_server.sendmail(sender, recipient, message)
     smtp_server.close()
-    
+
+# saves all articles which mentioned
+# as url in root_url
+# return text including all saved 
+# articles and all potential errors
 def save_articles_sqllite(root_url):
     initilize_database()
     articles_urls = find_all_articles(root_url)
     connection = sqlite3.connect("articles.db")
     articles_quantity = len(articles_urls)
-    text = 'start downloading at: ' 
-    text = text + str(datetime.datetime.now()) + '...\n'
-    for i in range (0,articles_quantity):
-        url = articles_urls[i]
-        text = text + str(i+1)+'/'+str(articles_quantity)+ ' ' + url + '\n'
-        article_array = create_article_dictionary(url)
-        connection = add_article(connection, article_array)
-    now = str(datetime.datetime.now())
-    text = text + 'downloading successfully finished at ' + now
-    return text
+    now = str(datetime.datetime.now()) + '\n'
+    log = '----- \n start downloading at: ' + now
+    try:
+        for i in range (0,articles_quantity):
+            url = articles_urls[i]
+            log = log + str(i+1)+'/'+str(articles_quantity)+ ' ' + url + '\n'
+            article_array = create_article_dictionary(url)
+            connection = add_article(connection, article_array)
+        message = now + ' ' + str(articles_quantity) + ' articles successfully downloaded'
+    except:
+        message = now + 'ERROR: \n'
+        e = sys.exc_info()[0]
+        message = message + str(e) + ' at ' + url
+    logging(log)
+    return message
+
+# write log to file
+def logging(text):
+    file = open('bild_harvester.log','w') 
+    file.write(text) 
+    file.close()
 
 main()

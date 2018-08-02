534a1055d419a367339cb84897df7bb211367caa
Guter Stiel <opi70@web.de>
2018-08-02 13:40:01 +0200

    setting up cronjob

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-sudo apt-get install python-pip
-pip install beautifulsoup4
+sudo timedatectl set-timezone Europe/Berlin #change to your timezone
+sudo apt-get install python3
+sudo apt-get install python3-pip
+sudo apt-get install python3-bs4
 cd cron
 sudo chmod +x cron_pull.sh
 sudo crontab cronjob.txt
